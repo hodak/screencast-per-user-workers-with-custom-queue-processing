8d48b9b1d9d8984a19a63a8c2446031b92bc7c06
hodak <kaczor555@gmail.com>
2016-12-07 13:40:38 +0100

    cut 3

--- lib/worker.ex
+++ lib/worker.ex
@@ -9,8 +9,24 @@ defmodule Restaurant.Worker do
     GenServer.call(pid, {:call_action, action_handler, argument})
   end
 
-  def handle_call({:call_action, action_handler, argument}, _from, actions) do
-    response = action_handler.(argument)
-    {:reply, response, actions}
+  def handle_call({:call_action, action_handler, argument}, from, actions) do
+    Process.send_after(self, :try_running, 0)
+    action = {action_handler, argument, from}
+    {:noreply, [action | actions]}
+  end
+
+  def handle_info(:try_running, []) do
+    {:noreply, []}
+  end
+
+  def handle_info(:try_running, [{action_handler, _, _} | _] = actions) do
+    relevant_action? = fn({handler, _, _}) -> handler == action_handler end
+    relevant_actions = Enum.filter(actions, relevant_action?)
+
+    arguments = Enum.map(relevant_actions, fn({_, arg, _}) -> arg end) |> Enum.reverse
+    response = action_handler.(arguments)
+    Enum.each(relevant_actions, fn({_, _, from}) -> GenServer.reply(from, response) end)
+
+    {:noreply, Enum.reject(actions, relevant_action?)}
   end
 end
--- test/worker_test.exs
+++ test/worker_test.exs
@@ -13,6 +13,23 @@ defmodule Restaurant.WorkerTest do
 
   test "it runs an action with an argument", %{worker_pid: pid} do
     action = fn(x) -> x end
-    assert :hodor == Restaurant.Worker.call_action(pid, action, :hodor)
+    assert [:hodor] == Restaurant.Worker.call_action(pid, action, :hodor)
+  end
+
+  test "it squashes actions and calls handler with multiple arguments", %{worker_pid: pid} do
+    self_pid = self
+    action = fn(x) -> send self_pid, x end
+    Enum.each([:a, :b], fn(arg) -> spawn fn -> Restaurant.Worker.call_action(pid, action, arg) end end)
+    assert_receive [:a, :b]
+  end
+
+  test "it only squashes actions with the same handler", %{worker_pid: pid} do
+    self_pid = self
+    a1 = fn(args) -> send self_pid, args end
+    a2 = fn(args) -> send self_pid, args end
+    [{a1, :a}, {a2, :b}, {a1, :c}, {a2, :d}]
+    |> Enum.each(fn({handler, arg}) -> spawn fn -> Restaurant.Worker.call_action(pid, handler, arg) end end)
+    assert_receive [:a, :c]
+    assert_receive [:b, :d]
   end
 end
